8d3df373fc58dc53710dc86423358d9309dd9342
Tucker Bickler <tuckersworld@gmail.com>
2011-11-12 21:40:11 -0600

    added environment port

--- app.js
+++ app.js
@@ -34,5 +34,5 @@ app.get('/', function(req, res){
   });
 });
 
-app.listen(55457);
+app.listen(process.env.PORT || 3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
